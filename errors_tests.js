b1fe8d7b0e89633a183b2a95e1ca74d099e4002d
Alon Amster <masteram@gmail.com>
2013-09-29 21:59:11 +0200

    Update errors_tests.js
    Updated test to reflect API change.

--- errors_tests.js
+++ errors_tests.js
@@ -20,7 +20,7 @@ Tinytest.addAsync("Errors template works", function(test, done) {
   Meteor.setTimeout(function() {
     test.equal(Errors.collection.find({seen: false}).count(), 0);
     test.equal(Errors.collection.find({}).count(), 1);
-    Errors.clear();
+    Errors.clearSeen();
     
     test.equal(Errors.collection.find({seen: true}).count(), 0);
     done();
