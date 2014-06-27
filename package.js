ea4756879e59ce450e2a47c245afddd45fda5792
Tom Coleman <tom@thesnail.org>
2014-06-27 11:57:40 +1000

    Trying to get unibuild workng

--- package.js
+++ package.js
@@ -1,18 +1,22 @@
 Package.describe({
-  summary: "A pattern to display application errors to the user"
+  summary: "A pattern to display application errors to the user",
+  version: "0.2.1",
+  githubUrl: "https://github.com/tmeasday/meteor-errors.git"
 });
 
 Package.on_use(function (api, where) {
-  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+  // This doesn't seem to work
+  // api.versionsFrom('METEOR-CORE@0.9.0')
+  
+  api.use(['minimongo@1.0.0', 'mongo-livedata@1.0.0', 'templating@1.0.0'], 'client');
 
   api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
   
-  if (api.export) 
-    api.export('Errors');
+  api.export('Errors');
 });
 
 Package.on_test(function(api) {
-  api.use('errors', 'client');
+  api.use('tmeasday:errors', 'client');
   api.use(['tinytest', 'test-helpers'], 'client');  
   
   api.add_files('errors_tests.js', 'client');
