c3966e5a6fbcbc63799950a6a04527219023bfd8
Tom Coleman <tom@thesnail.org>
2014-06-27 11:49:45 +1000

    Updated test.

--- errors_tests.js
+++ errors_tests.js
@@ -10,18 +10,16 @@ Tinytest.add("Errors collection works", function(test) {
 Tinytest.addAsync("Errors template works", function(test, done) {  
   Errors.throw('A new error!');
   test.equal(Errors.collection.find({seen: false}).count(), 1);
-  
+
   // render the template
-  OnscreenDiv(Spark.render(function() {
-    return Template.meteorErrors();
-  }));
-  
+  UI.insert(UI.render(Template.meteorErrors), document.body);
+
   // wait a few milliseconds
   Meteor.setTimeout(function() {
     test.equal(Errors.collection.find({seen: false}).count(), 0);
     test.equal(Errors.collection.find({}).count(), 1);
     Errors.clearSeen();
-    
+
     test.equal(Errors.collection.find({seen: true}).count(), 0);
     done();
   }, 500);
