891983082eb6c2d484a82cfa2d9ea03a26645df6
Twiga <twigatime@gmail.com>
2014-11-02 18:17:13 -0500

    Using simpler versionsFrom call for package track

--- package.js
+++ package.js
@@ -9,17 +9,15 @@ Package.describe({
 });
 
 Package.onUse(function (api, where) {
-  api.use(['minimongo@1.0.5', 'mongo-livedata@1.0.6', 'templating@1.0.9'], 'client');
-
+  api.versionsFrom('METEOR@0.9.4');
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
   api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
 
-  if (api.export)
-    api.export('Errors');
+  api.export('Errors');
 });
 
 Package.onTest(function(api) {
   api.use('twigatech:errors', 'client');
   api.use(['tinytest', 'test-helpers'], 'client');
-
   api.addFiles('errors_tests.js', 'client');
 });
