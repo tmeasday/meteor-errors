0df1ef32026622e8cbb53a5f29e6c9173566a25e
Twiga <twigatime@gmail.com>
2014-11-02 17:59:43 -0500

    Updated package.js for 1.0 Meteor Packaging system

--- package.js
+++ package.js
@@ -1,19 +1,25 @@
 Package.describe({
-  summary: "A pattern to display application errors to the user"
+  summary: "A pattern to display application errors to the user",
+  version: "0.2.1",
+  name: "twigatech:errors",
+  description: "A pattern to display application errors to the user",
+  homepage: "https://github.com/twigatech/meteor-errors",
+  author: "Joel Braun <joel.m.braun@gmail.com>",
+  git: "https://github.com/twigatech/meteor-errors.git"
 });
 
-Package.on_use(function (api, where) {
-  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+Package.onUse(function (api, where) {
+  api.use(['minimongo@1.0.5', 'mongo-livedata@1.0.6', 'templating@1.0.9'], 'client');
 
-  api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
-  
-  if (api.export) 
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+
+  if (api.export)
     api.export('Errors');
 });
 
-Package.on_test(function(api) {
-  api.use('errors', 'client');
-  api.use(['tinytest', 'test-helpers'], 'client');  
-  
-  api.add_files('errors_tests.js', 'client');
+Package.onTest(function(api) {
+  api.use('twigatech:errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.addFiles('errors_tests.js', 'client');
 });
